8c936fe786572857f9cee29eb1e40d3815d4ad58
RONALD FABRIZIO HURTADO RAMOS <maxx1212_2@hotmail.com>
2025-11-28 23:58:33 -0500

    Remove unused user authentication route from API routes

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,7 @@ use App\Http\Controllers\MetricsController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 
 Route::prefix('v1/marketing/metrics')->group(function () {
     Route::get('post/{postId}', [MetricsController::class, 'getMetrics']);
